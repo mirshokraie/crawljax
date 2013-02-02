816c05aa0007a623ca180fb7cda461184617c699
Shabnam <mirshokraie@gmail.com>
2013-02-02 00:23:42 -0800

    more changes

--- core/src/main/java/com/crawljax/globals/LabeledFunctions.java
+++
@@ -1,9 +0,0 @@
-package com.crawljax.globals;
-
-import java.util.HashMap;
-
-public class LabeledFunctions {
-	/* map<label name, function name> */
-	public static  HashMap<String,String> labeledFunctions=new HashMap<String,String>();
-
-}
